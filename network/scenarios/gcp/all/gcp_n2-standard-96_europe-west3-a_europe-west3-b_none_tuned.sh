a6a459390bc10b008fb523b9323390b6c182dccc
Oke Hermanns <okehermanns@Okes-MacBook-Air.local>
2026-07-09 13:06:05 +0200

    use n2-standard-96 machines for better comparability with existing data

--- network/scenarios/gcp/all/gcp_n2-standard-96_europe-west3-a_europe-west3-b_none_tuned.sh
+++ network/scenarios/gcp/all/gcp_n2-standard-96_europe-west3-a_europe-west3-b_none_tuned.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 source "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)/../common.inc"
 
-SCENARIO_NAME=gcp_n2-standard-48_europe-west3-a_europe-west3-b_different-host_tuned
+SCENARIO_NAME=gcp_n2-standard-96_europe-west3-a_europe-west3-b_different-host_tuned
 OS_TUNING=network-throughput
 INSTANCE_AFFINITY=none
-CLIENT_MACHINE_TYPE=n2-standard-48
-SERVER_MACHINE_TYPE=n2-standard-48
+CLIENT_MACHINE_TYPE=n2-standard-96
+SERVER_MACHINE_TYPE=n2-standard-96
 CLIENT_AVAILABILITY_ZONE=europe-west3-a
 SERVER_AVAILABILITY_ZONE=europe-west3-b
 PLACEMENT_MODE=multi-az
